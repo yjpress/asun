b349e540b8321d4abc0a117958960c2d0bca3f16
yjpress <yjpress@hanmail.net>
2020-12-26 15:59:33 +0900

    react message

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>asun- 2020-12-10</p>
+        <p>asun- 2020-12-10 and start react</p>
       </header>
     </div>
   );
